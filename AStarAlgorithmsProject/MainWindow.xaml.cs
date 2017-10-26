ae9f5255b8f6553d0d908ddbd54561354137aa81
ajkoves <ajkoves@yahoo.com>
2017-10-26 14:07:07 -0400

    Update

--- AStarAlgorithmsProject/MainWindow.xaml.cs
+++ AStarAlgorithmsProject/MainWindow.xaml.cs
@@ -21,29 +21,58 @@ namespace AStarAlgorithmsProject
     public partial class MainWindow : Window
     {
         //MainDriver main;
+        Canvas parent;
+        Grid map;
+        List<ColumnDefinition> columns;
+        List<RowDefinition> rows;
+        Rectangle[,] tiles;
 
         public MainWindow()
         {
-            /*InitializeComponent();
-            textBox.IsReadOnly = true;
+            InitializeComponent();
+            /*textBox.IsReadOnly = true;
             textBox.TextAlignment = TextAlignment.Center;
 
             main = new MainDriver(); // creates an instance of Main Drive for us to reference whenever the user changes something about the display and get results from.
             textBox.Text = main.TestMap(); // shows results of the algorithim, currenttly uses hardcoded start and end positions.
         */
+            
             int size = 10;
+            parent = new Canvas();
+            InitializeBackground(parent);
+            Set_Up_Map(size);
+            Create_Buttons();
+            
+            this.Content = parent;
+        }
 
-            Grid map = new Grid();
+        void InitializeBackground (Canvas c)
+        {
+            c.Width = 1;
+            c.Height = 1;
+            c.HorizontalAlignment = HorizontalAlignment.Center;
+            c.VerticalAlignment = VerticalAlignment.Center;
+
+        }
+
+        /// <summary>
+        /// Generates the grid (size x size) and fills it with rectangles.
+        /// </summary>
+        /// <param name="size"></param>
+        void Set_Up_Map(int size)
+        {
+            map = new Grid();
+            parent.Children.Add(map);
             map.Background = Brushes.Black;
-            map.HorizontalAlignment = HorizontalAlignment.Center;
-            map.VerticalAlignment = VerticalAlignment.Center;
+            //map.HorizontalAlignment = HorizontalAlignment.Center;
+            //map.VerticalAlignment = VerticalAlignment.Center;
             map.ShowGridLines = true;
             map.Height = 500;
             map.Width = 500;
 
-            List<ColumnDefinition> columns = new List<ColumnDefinition>();
-            List<RowDefinition> rows = new List<RowDefinition>();
-            Rectangle[,] tiles = new Rectangle[size,size];
+            columns = new List<ColumnDefinition>();
+            rows = new List<RowDefinition>();
+            tiles = new Rectangle[size, size];
 
             for (int i = 0; i < size; i++)
             {
@@ -56,21 +85,32 @@ namespace AStarAlgorithmsProject
 
             for (int i = 0; i < size; i++)
                 for (int j = 0; j < size; j++)
-                { 
-                    tiles[i,j] = new Rectangle();
-                    tiles[i,j].Fill = Brushes.LightGray;
+                {
+                    tiles[i, j] = new Rectangle();
+                    tiles[i, j].Fill = Brushes.LightGray;
                     tiles[i, j].MouseDown += Mouse_Clicked;
-                    map.Children.Add(tiles[i,j]);
+                    map.Children.Add(tiles[i, j]);
                     Grid.SetColumn(tiles[i, j], i);
-                    Grid.SetRow(tiles[i, j],j);
+                    Grid.SetRow(tiles[i, j], j);
                 }
-
-            this.Content = map;
+            
         }
 
         void Mouse_Clicked(object sender, MouseButtonEventArgs e)
         {
-            
+            Rectangle r = (Rectangle)sender;
+            r.Fill = Brushes.DarkOliveGreen;
+               
+        }
+
+        private void Create_Buttons()
+        {
+            RadioButton r = new RadioButton();
+            parent.Children.Add(r);
+            r.HorizontalAlignment = HorizontalAlignment.Right;
+            r.VerticalAlignment = VerticalAlignment.Center;
+            r.Content = "Start";
+
         }
     }
 }
