34335850e2643d85d4b73c364208fc9c13e8e0e1
Nick Main <nickmaster24@gmail.com>
2017-11-22 20:58:05 -0500

    Added helpful variables and comments to the TimeAnalyzer

--- AStarAlgorithmsProject/Driver/TimeAnalyzer.cs
+++ AStarAlgorithmsProject/Driver/TimeAnalyzer.cs
@@ -12,34 +12,38 @@ namespace AStarAlgorithmsProject.Driver
 
         static void Main(string[] args)
         {
-            TileMap tileMap = new TileMap(200);
+            int mapSize = 100; //Change this to change the test map size
+
+            TileMap tileMap = new TileMap(mapSize);
             tileMap.InitMap();
             tileMap.SetStart(new Point(1, 1));
 
-            for (int i = 1; i < 200; i += 2)
+            for (int i = 1; i < mapSize; i += 2)
             {
-                for (int o = 0; o < 200; o++)
+                for (int o = 0; o < mapSize; o++)
                 {
-                    if ((i + o) % 100 != 0) //Make openings in the lines
+                    if ((i + o) % mapSize != 0) //Make openings in the lines
                     {
                         tileMap.SetPassable(new Point(i, o), false);
                     }
                 }
             }
 
-            Solver solver = new AStarSolver();
+            Solver solver = new AStarSolver(); //Change this to change which algorithm is used
 
-            for (int i = 80; i < 200; i += 10)
+            for (int i = 20; i < mapSize; i += 20) // You can mess with this to change how long the paths end up being. i should be a mutliple of 2
             {
                 tileMap.SetGoal(new Point(i, i));
 
                 System.Console.WriteLine();
                 TileMap freshMap = new TileMap(tileMap);
                 freshMap.ResetStates();
-                System.Console.WriteLine(solver.getPath(freshMap).Count + " tiles long"); 
+                System.Console.WriteLine(solver.getPath(freshMap).Count + " tiles long");
+
+                short testTrials = 10; //Change this to change the sample size
 
                 long total = 0;
-                for (int x = 1; x <= 10; x++)
+                for (int x = 1; x <= testTrials; x++) 
                 {
                     freshMap = new TileMap(tileMap);
                     freshMap.ResetStates();
@@ -48,7 +52,7 @@ namespace AStarAlgorithmsProject.Driver
                     total += stopwatch.ElapsedMilliseconds;
                 }
 
-                System.Console.WriteLine(total / 10 + " ms"); 
+                System.Console.WriteLine(total / testTrials + " ms"); 
             }
         }
     }
